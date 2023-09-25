99c2d86b4862045aceb9e3a268baced62c72e7f3
vinods029 <vinods029@gmail.com>
2023-09-25 22:24:43 +0530

    added custom hooks

---
+++ src/hooks/usePokemonList.js
@@ -0,0 +1,64 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+function usePokemonList(){
+
+    
+    const DEFAULT_URL = "https://pokeapi.co/api/v2/pokemon"
+
+
+
+    const [pokemonListState,setPokemonListState] = useState({
+        pokemonList:[],
+        pokedexUrl:DEFAULT_URL,
+        nextUrl:DEFAULT_URL,
+        prevUrl:DEFAULT_URL
+    })
+
+
+    async function downloadPokemons(){
+        const response = await axios.get(pokemonListState.pokedexUrl ?  pokemonListState.pokedexUrl : DEFAULT_URL)
+        
+        const pokemonResults = response.data.results // Array of pokemons
+
+        // setNextUrl(response.data.next)
+        // setPrevUrl(response.data.previous)
+        // setPokemonListState((state)=>({...state, nextUrl:response.data.next, prevUrl:response.data.previous}))
+
+        const pokemonPromise =  pokemonResults.map((pokemon)=>
+        axios.get(pokemon.url))
+
+        const pokemonListData = await axios.all(pokemonPromise) 
+        
+        
+        const pokemonFinalList = pokemonListData.map(pokemonData=>{
+            const pokemon = pokemonData.data
+            return{
+                id:pokemon.id,
+                name:pokemon.name,
+                image: pokemon.sprites.other.dream_world.front_default,
+                types: pokemon.types
+
+            }
+        })
+
+        // setPokemonList(pokemonFinalList)
+        setPokemonListState({...pokemonListState, pokemonList: pokemonFinalList,nextUrl:response.data.next, prevUrl:response.data.previous});
+
+   
+        
+
+    }
+   
+  
+    useEffect(()=>{
+        downloadPokemons();
+       
+    },[pokemonListState.pokedexUrl]);
+
+    return[pokemonListState,setPokemonListState ];
+
+}
+
+
+export default usePokemonList;
